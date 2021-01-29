c9a4cf9d79560e2ab7d4dd516755329ce406db74
DESKTOP-2MRQV0F\COPC <thanawan2131@gmail.com>
2021-01-29 17:04:04 +0700

    send assignment week 1

---
+++ Netwhore/Assets/ScriptsFindinh.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptsFindinh : MonoBehaviour
+{
+
+    public InputField inputField;
+    public Text Result; 
+   
+    
+    
+    public void finding()
+    {
+        var Word = new List<string>() { "Nice", "Inuit", "Guy", "Giggle", "Aatrox" };
+       foreach (string word in Word)
+        {
+            if (word == inputField.text)
+            {
+                Result.text = "<color=green>" + inputField.text + "</color>" + " is found";
+                return;
+            }
+            else
+            {
+                Result.text = "<color=red>" + inputField.text + "</color>" + " is not found";
+            }
+        }
+    }
+
+    
+
+
+   
+       
+    
+       
+
+
+
+    
+
+
+
+
+
+}
